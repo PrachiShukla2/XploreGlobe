5afc7e0a8824316da27994f7986803c64c202cf3
PrachiShukla2 <prachishuklamount@gmail.com>
2024-04-18 20:40:35 +0530

    kj

--- js/searchResults.js
+++ js/searchResults.js
@@ -14,3 +14,27 @@ export const buildSearchResults = (resultArray) =>{
       searchResults.append(resultItem);
     });
 }
+
+const createResultItem = (result)=>{
+    const  resultItem = document.createElement("div");
+    resultItem.classList.add("resultItem");
+    const resultTitle = document.createElement("div");
+    resultTitle.classList.add("resultTitle");
+    const link = document.createElement("a");
+    link.href = `https://en.wikipedia.org/?curid=${result.id}`;
+    link.textContent = result.title;
+    link.target = "_blank";
+    resultTitle.append(link);
+    resultItem.append(resultTitle);
+    return resultItem;
+}
+const createResultImage = (result)=>{
+ const resultImage = document.createELement("div");
+ resultImage.classList.add("resultImage");
+ const img = document.createElement("img");
+ img.src = result.img;
+ img.alt = result.title;
+ resultImage.append(img);
+ return resultImage;
+
+}
